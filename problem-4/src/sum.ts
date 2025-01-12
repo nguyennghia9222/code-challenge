e561b43873bb7c27be1095e04cc0e2d5add831c9
nguyennghia9222 <code.nguyennghia@gmail.com>
2025-01-12 10:43:06 +0700

    init source

---
+++ problem-4/src/sum.ts
@@ -0,0 +1,13 @@
+const sumToNumber1 = (n: number): number => {
+  return (n * (n + 1)) / 2;
+};
+
+const sumToNumber2 = (n: number): number => {
+  return (n * (n + 1)) / 2;
+};
+
+const sumToNumber3 = (n: number): number => {
+  return (n * (n + 1)) / 2;
+};
+
+export { sumToNumber1, sumToNumber2, sumToNumber3 };
---
+++ problem-4/tests/sum.test.ts
@@ -0,0 +1,19 @@
+import { sumToNumber1, sumToNumber2, sumToNumber3 } from "../src/sum";
+
+describe("sumToNumber1", () => {
+  test("should return a summation from 1 to n", () => {
+    expect(sumToNumber1(5)).toBe(15);
+  });
+});
+
+describe("sumToNumber2", () => {
+  test("should return a summation from 1 to n", () => {
+    expect(sumToNumber1(5)).toBe(15);
+  });
+});
+
+describe("sumToNumber3", () => {
+  test("should return a summation from 1 to n", () => {
+    expect(sumToNumber1(5)).toBe(15);
+  });
+});

a1a947cbafb5a62331a37314ca1a296ad6647fd4
nguyennghia9222 <code.nguyennghia@gmail.com>
2025-01-16 07:27:08 +0700

    update code

--- problem-5/src/db.ts
+++ problem-5/src/db.ts
@@ -1,7 +1,10 @@
 import { connect as mongooseConnect, disconnect as mongooseDisconnect } from "mongoose";
 
+const dbHost = process.env.DB_HOST || "127.0.0.1";
+const dbPort = process.env.DB_PORT || 3000;
+
 const connect = async () => {
-  await mongooseConnect("mongodb://mongodb:27017/code-challenge");
+  await mongooseConnect(`mongodb://${dbHost}:${dbPort}/code-challenge`);
 };
 
 const disconnect = async () => {
